31286483c7421e71c40a8b2454d7030fb862c3dc
Lang Martin <lmartin@opentable.com>
2015-03-10 12:40:03 -0400

    project version 1.2

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject org.clojars.langmartin/lein-git-version "1.1"
+(defproject org.clojars.langmartin/lein-git-version "1.2.0"
   :description "Use git for project versions"
   :url "https://github.com/cvillecsteele/lein-git-version"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
+  ;; :plugins [[org.clojars.langmartin/lein-git-version "1.2.0"]]
   :profiles
   {:dev {:dependencies [[midje "1.4.0"]]
          :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
