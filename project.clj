70dad91db4b0f34cc0922f85592e15835bf32c29
Lang Martin <lmartin@opentable.com>
2015-03-10 12:40:49 -0400

    project version dogfood

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.langmartin/lein-git-version "1.2.0"
+(defproject org.clojars.langmartin/lein-git-version "GIT-VERSION"
   :description "Use git for project versions"
   :url "https://github.com/cvillecsteele/lein-git-version"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  ;; :plugins [[org.clojars.langmartin/lein-git-version "1.2.0"]]
+  :plugins [[org.clojars.langmartin/lein-git-version "1.2.0"]]
   :profiles
   {:dev {:dependencies [[midje "1.4.0"]]
          :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
